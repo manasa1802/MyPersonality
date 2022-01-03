fcc3aca1fdc49e53ad4f94b62b28c8b7085d5421
manasa tr <manasatr1802@gmail.com>
2022-01-03 15:50:37 -0800

    App icons and background image added

---
+++ MyPersonality/Model/MyPersonalityModel.swift
@@ -0,0 +1,13 @@
+//
+//  MyPersonalityModel.swift
+//  MyPersonality
+//
+//  Created by manasa on 12/12/21.
+//  Copyright © 2021 manasa. All rights reserved.
+//
+
+import Foundation
+
+struct MyPersonalityModel {
+    <#fields#>
+}
